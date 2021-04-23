1ff631e1e5fe98b391aa2def5b7b6ed05ee226f5
GuseynovIbragim <78927590+GuseynovIbragim@users.noreply.github.com>
2021-04-23 14:26:44 +0300

    Гусейнов

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,11 +3,108 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
+string pst;
+pst = "";
+stack <char> stackOperators;
+for (int i = 0; i < inf.size(); i++) {
+    if (isdigit(inf[i])) {
+        //while (!IsOperator(inf[i])) {
+        while (isdigit(inf[i])) {
+            pst += inf[i];
+            i++;
+            if (i == inf.size())
+                break;
+        }
+        pst += " ";
+        i--;
+    }
+    if (IsOperator(inf[i])) {
+        if (inf[i] == '(')
+            stackOperators.push(inf[i]);
+        else if (inf[i] == ')') {
+            char s = stackOperators.top();
+            stackOperators.pop();
+            while (s != '(') {
+                //pst += to_string(s);
+                pst += string() + s;
+                pst += ' ';
+                s = stackOperators.top();
+                stackOperators.pop();
+            }
+        }
+        else {
+            if (!stackOperators.empty()) {
+                if (Priority(inf[i]) <= Priority(stackOperators.top())) {
+                    //pst += to_string(stackOperators.top());
+                    pst += string() + stackOperators.top();
+                    pst += ' ';
+                    stackOperators.pop();
+                }
+            }
+            stackOperators.push(inf[i]);
+        }
+    }
+}
+while (!stackOperators.empty()) {
+    pst += stackOperators.top();
+    pst += ' ';
+    stackOperators.pop();
+}
   return std::string("");
 }
 
 int eval(std::string pst) {
-  // добавьте сюда нужный код
+int result = 0;
+stack <int> buff;
+for (int i = 0; i < pst.size(); i++) {
+    if (isdigit(pst[i])) {
+        string strBuff = "";
+        //while (!IsOperator(pst[i])) {
+        while (isdigit(pst[i])) {
+            strBuff += pst[i];
+            i++;
+            if (i == pst.size())
+                break;
+        }
+        buff.push(stoi(strBuff));
+        i--;
+    }
+    else if (IsOperator(pst[i])) {
+        int a = buff.top();
+        buff.pop();
+        int b = buff.top();
+        buff.pop();
+        switch (pst[i]) {
+        case '+': result = b + a; break;
+        case '-': result = b - a; break;
+        case '*': result = b * a; break;
+        case '/': result = b / a; break;
+        case '^': result = stoi(to_string(pow(stoi(to_string(b)),stoi(to_string(a))))); break;
+        }
+        buff.push(result);
+    }
   return 0;
+}
+return buff.top();
+}
+
+int Priority(char s)
+{
+switch (s) {
+case '(': return 0;
+case ')': return 1;
+case '+': return 2;
+case '-': return 3;
+case '*': return 4;
+case '/': return 4;
+case '^': return 5;
+default: return 6;
+}
+}
+
+bool IsOperator(char symbol) {
+if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '^' || symbol == '(' || symbol == ')')
+    return true;
+else
+    return false;
 }
